c16ca40e4e1aed72dce0868c2328f963f675e295
alexr <belloa@hartwick.edu>
2020-04-07 12:22:54 -0400

    Palindrome

--- Driver.py
+++ Driver.py
@@ -1,1 +1,15 @@
+# Alex Bello
+# 4/7/2020
+# The driver program for seeing if a user input a palindrome
+from Methods import palindrome_check
+from Methods import punccheck
 
+
+print("Please enter what you think is a palindrome.")
+line = input()
+punccheck(line)
+
+if palindrome_check(line):
+    print("You got it! That's a palindrome.")
+else:
+    print("Sorry, that's not a palindrome.")
--- Methods.py
+++ Methods.py
@@ -1,1 +1,28 @@
+# Alex Bello
+# 4/7/2020
+# The methods for the palindrome program
+
+
+def palindrome_check(line):
+    line = line.lower()
+    line = line.strip()
+    length = int(len(line))
+    if length == 0 or length == 1:
+        return True
+    elif line[0] != line[-1]:
+        return False
+    else:
+        return palindrome_check(line[1:-1])
+
+
+def punccheck(line):
+    # Declaring what the punctuation marks are
+    punctuation_marks = '''!@#$%^&*()-;:'"/?.,><'''
+    for x in line.lower():
+        if x in punctuation_marks:
+            line = line.replace(x, "")
+    # This will reprint the string without the punctuation marks
+    print(line)
+
+
 
